6f59b22c274d5b7da3e7e1faba90abdc21f83c0d
Caio <caiobsouza@hotmail.com>
2017-11-20 00:10:44 -0300

    Extending Tray class e refactoring

---
+++ app/timer-tray.js
@@ -0,0 +1,39 @@
+const electron = require('electron');
+const path = require('path');
+const { Tray } = electron;
+
+class TimerTray extends Tray {
+    constructor(iconPath, window) {
+        super(iconPath);
+        
+        this.window = window;
+        this.setToolTip('Tasky!');
+        this.on('click', this.onClick.bind(this));
+    }
+
+    getIconPath() {
+        const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+        return path.join(__dirname, `./src/assets/${iconName}`);
+    }
+
+    onClick(event, bounds) {
+        const { x, y } = bounds;
+        const { height, width } = this.window.getBounds();
+
+        if (this.window.isVisible()) {
+            this.window.hide();
+        } else {
+
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            this.window.setBounds({
+                x: Math.floor(x - width / 2),
+                y: Math.floor(yPosition),
+                height,
+                width
+            });
+            this.window.show();
+        }
+    }
+}
+
+module.exports = TimerTray;
--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+const path = require('path');
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const TimerTray = require('./app/timer-tray');
 
+const { app, BrowserWindow, Tray } = electron;
+/**
+ * @type {BrowserWindow}
+ */
 let mainWindow;
 
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({});
+/**
+ * @type {TimerTray}
+ */
+let tray;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        height: 500,
+        width: 250,
+        frame: false,
+        resizable: false,
+        show: false
+    });
+
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+}
+
+function configureTray() {
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+
+    tray = new TimerTray(iconPath, mainWindow);
+}
+
+app.on('ready', () => {
+    createWindow();
+    configureTray();
 });
